2185d82fe45a275f5ce20c1ea1b7311698b58403
Aishiia <aishiia278@gmail.com>
2024-01-25 09:49:26 +0700

    make splashscreen

---
+++ lib/ui/home_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+// TODO 2: MAKE HOME SCREEN
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
